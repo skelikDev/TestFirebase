15170f1573d0a3ab55dc78633bab0c73caff724a
Aleksandr Kim <skelikDev@gmail.com>
2021-09-02 15:25:49 +0300

    Manual rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  base: '/'
+    plugins: [reactRefresh()],
+    build: {
+        chunkSizeWarningLimit: 500,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 })
